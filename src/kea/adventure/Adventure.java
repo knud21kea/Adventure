7ab8b0c121a62f0e29286ff1aa753e07da0c37f9
knud21kea <grah0070@stud.kea.dk>
2021-09-25 12:58:02 +0200

    Move-room control moved back to Adventure class.

--- src/kea/adventure/Adventure.java
+++ src/kea/adventure/Adventure.java
@@ -4,20 +4,22 @@ import java.util.Scanner;
 
 public class Adventure {
 
-    public static Room currentRoom; //not sure this should be static but fails otherwise
 
     public static void main(String[] args) {
 
         //create all instances of room objects - probably should be an array - data very basic
-        Room room1 = new Room("Room1", "You see a 1");
-        Room room2 = new Room("Room2", "You see a 2");
-        Room room3 = new Room("Room3", "You see a 3");
-        Room room4 = new Room("Room4", "You see a 4");
-        Room room5 = new Room("Room5", "You see a 5");
-        Room room6 = new Room("Room6", "You see a 6");
-        Room room7 = new Room("Room7", "You see a 7");
-        Room room8 = new Room("Room8", "You see a 8");
-        Room room9 = new Room("Room9", "You see a 9");
+        Room room1 = new Room("Room1", "Looks like an entrance.");
+        Room room2 = new Room("Room2", "Not much to see.");
+        Room room3 = new Room("Room3", "The way straight seems blocked");
+        Room room4 = new Room("Room4", "Looks empty");
+        Room room5 = new Room("Room5", "This room looks special.");
+        Room room6 = new Room("Room6", "You see nothing interesting");
+        Room room7 = new Room("Room7", "What is here?...Oh, nothing.");
+        Room room8 = new Room("Room8", "Seems to be several exits");
+        Room room9 = new Room("Room9", "No beer here");
+
+        Room currentRoom = room1;
+        Room requestedRoom = null;
 
         //Make connections - not auto 2 way and not with map - todo?
         room1.setEast(room2);
@@ -39,8 +41,6 @@ public class Adventure {
         room4.setNorth(room1);
         room1.setSouth(room4);
 
-        Room currentRoom = room1;
-
         Scanner input = new Scanner(System.in);
 
         //Intro and description of start room
@@ -48,7 +48,9 @@ public class Adventure {
 
         //Get inputs until user types exit or x
         String menuOption = "Z";
+        ;
         while (!menuOption.equals("X") && !menuOption.equals("EXIT")) {
+            requestedRoom = currentRoom; //used to only print blocked if user tries a blocked route
             System.out.print(currentRoom.getRoomName() + ": ");
             System.out.println(currentRoom.getRoomDescription());
             System.out.print("What do you want to do? ");
@@ -56,14 +58,19 @@ public class Adventure {
 
             //construction is very messy but works. Improvements needed here. Eliminate static variable?
             switch (menuOption) {
-                case "GO NORTH", "NORTH", "N" -> currentRoom=currentRoom.goNorth(currentRoom);
-                case "GO EAST", "EAST", "E" -> currentRoom=currentRoom.goEast(currentRoom);
-                case "GO SOUTH", "SOUTH", "S" -> currentRoom=currentRoom.goSouth(currentRoom);
-                case "GO WEST", "WEST", "W" -> currentRoom=currentRoom.goWest(currentRoom);
+                case "GO NORTH", "NORTH", "N" -> requestedRoom = currentRoom.getNorthRoom();
+                case "GO EAST", "EAST", "E" -> requestedRoom = currentRoom.getEastRoom();
+                case "GO SOUTH", "SOUTH", "S" -> requestedRoom = currentRoom.getSouthRoom();
+                case "GO WEST", "WEST", "W" -> requestedRoom = currentRoom.getWestRoom();
                 case "LOOK", "L" -> System.out.print("OK. ");
                 case "HELP", "H" -> getHelp();
                 case "EXIT", "X" -> endMessage();
                 default -> unknownCommand(menuOption);
+            }
+            if (requestedRoom == null) {
+                System.out.println("That way is blocked.");
+            } else {
+                currentRoom = requestedRoom;
             }
         }
     }
@@ -82,11 +89,11 @@ public class Adventure {
 
     //Give up
     public static void endMessage() {
-        System.out.println("Hope to see you again");
+        System.out.println("Really? Hope to see you again soon.");
     }
 
     //Invalid input
     public static void unknownCommand(String menuOption) {
-        System.out.println("I do not understand \"" + menuOption + "\"");
+        System.out.println("I do not understand \"" + menuOption + "\".");
     }
 }
--- src/kea/adventure/Room.java
+++ src/kea/adventure/Room.java
@@ -53,45 +53,4 @@ public class Room {
     public void setWest(Room west) {
         this.west = west;
     }
-
-
-    public Room goNorth(Room currentRoom) {
-        if (currentRoom.getNorthRoom() != null) {
-            System.out.println("Going North");
-            return currentRoom.getNorthRoom();
-        } else {
-            System.out.println("You cannot go that way");
-            return currentRoom;
-        }
-    }
-
-    public Room goEast(Room currentRoom) {
-        if (currentRoom.getEastRoom() != null) {
-            System.out.println("Going East");
-            return currentRoom.getEastRoom();
-        } else {
-            System.out.println("You cannot go that way");
-            return currentRoom;
-        }
-    }
-
-    public Room goSouth(Room currentRoom) {
-        if (currentRoom.getSouthRoom() != null) {
-            System.out.println("Going South");
-            return currentRoom.getSouthRoom();
-        } else {
-            System.out.println("You cannot go that way");
-            return currentRoom;
-        }
-    }
-
-    public Room goWest(Room currentRoom) {
-            if (currentRoom.getWestRoom() != null) {
-                System.out.println("Going West");
-                return currentRoom.getWestRoom();
-            }else {
-                System.out.println("You cannot go that way");
-                return currentRoom;
-            }
-    }
 }
